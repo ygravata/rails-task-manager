2d67f80f1757f65e540c60db13b9755d43940f84
Yuri Gravata <ygravata@gmail.com>
2020-04-18 12:44:43 -0300

    forgot to commit earlier

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,33 @@
+class TasksController < ApplicationController
+  # Create (GET and POST)
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to task_path(@task)
+  end
+
+  # Read all
+  def index
+    @tasks = Task.all
+  end
+
+  # Read one
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  # Update
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # Create (get, post)
+  get 'tasks/new', to: 'tasks#new'
+  post 'tasks', to: 'tasks#create'
+  # Read all
+  get 'tasks', to: 'tasks#index'
+  # Read one
+  get 'tasks/:id', to: 'tasks#show', as: :task
+  # Update
+  get 'tasks/:id/edit', to: 'tasks#edit'
 end
